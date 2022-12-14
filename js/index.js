ddd757100dbaba38f702f8f04d2625fe67a00a0f
Faith Moraa <moraaf770@gmail.com>
2022-12-14 15:19:47 +0300

    Canges in JS

--- js/index.js
+++ js/index.js
@@ -119,7 +119,7 @@ let currentVisibleFilm = {}
 
         const filmTicketsSold = document.createElement('h6')
         filmTicketsSold.classList.add('card-text')
-        filmTicketsSold.innerText = `Availabe Tickets: ${tickets_sold}`
+        filmTicketsSold.innerText = `Availabe Tickets: ${capacity-tickets_sold}`
 
         const filmDescription = document.createElement('p')
         filmDescription.classList.add('card-text')
